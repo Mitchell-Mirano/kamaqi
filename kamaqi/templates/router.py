724ba6341c90f696ce68a5f30182b55ee985d5b5
Mitchell Mirano <mitchellmirano25@gmail.com>
2023-02-09 00:01:30 -0500

    add Session type and status_code in the router template

--- kamaqi/templates/router.py
+++ kamaqi/templates/router.py
@@ -19,37 +19,41 @@ from {{app}}s.crud import delete_{{app}}_in_db
 
 @{{app}}s_routs.post(path="/create/",
                      tags=["{{app.capitalize()}}s"],
-                     status_code=status.HTTP_201_CREATED,
-                     response_model={{app.capitalize()}}Read)
+                     response_model={{app.capitalize()}}Read,
+                     status_code=status.HTTP_201_CREATED)
 async def create_{{app}}({{app}}_data:{{app.capitalize()}}Create,
-                         db=Depends(get_db)):
+                         db:Session=Depends(get_db)):
 
     return insert_{{app}}(db,{{app}}_data)
 
 @{{app}}s_routs.get(path="/get/all/",
                     tags=["{{app.capitalize()}}s"],
-                    response_model=List[{{app.capitalize()}}Read])
-async def get_all_{{app}}s(db=Depends(get_db)):
+                    response_model=List[{{app.capitalize()}}Read],
+                    status_code=status.HTTP_200_OK)
+async def get_all_{{app}}s(db:Session=Depends(get_db)):
     return select_all_{{app}}s(db)
 
 @{{app}}s_routs.get(path="/get/{id}/",
                     tags=["{{app.capitalize()}}s"],
-                    response_model={{app.capitalize()}}Read)
-async def get_{{app}}(id:int,db=Depends(get_db)):
+                    response_model={{app.capitalize()}}Read,
+                    status_code=status.HTTP_200_OK)
+async def get_{{app}}(id:int,db:Session=Depends(get_db)):
     return select_{{app}}_by_id(db,id)
 
 @{{app}}s_routs.put(path="/update/{id}/",
                 tags=["{{app.capitalize()}}s"],
-                response_model={{app.capitalize()}}Read)
+                response_model={{app.capitalize()}}Read,
+                status_code=status.HTTP_200_OK)
 async def update_{{app}}(id:int,
                         {{app}}_data:{{app.capitalize()}}Update,
-                        db=Depends(get_db)):
+                        db:Session=Depends(get_db)):
 
     return update_{{app}}_in_db(db,id,{{app}}_data)
 
 @{{app}}s_routs.delete(path="/delete/{id}/",
-                    tags=["{{app.capitalize()}}s"])
-async def delete_{{app}}(id:int,db=Depends(get_db)):
+                    tags=["{{app.capitalize()}}s"],
+                    status_code=status.HTTP_200_OK)
+async def delete_{{app}}(id:int,db:Session=Depends(get_db)):
 
     delete_{{app}}_in_db(db,id)
 
