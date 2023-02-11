3052c6a92756babcee8489709265037364794bc5
Mitchell Mirano <mitchellmirano25@gmail.com>
2023-02-11 15:59:43 -0500

    show apps now print a table

--- kamaqi/show/show.py
+++ kamaqi/show/show.py
@@ -1,5 +1,6 @@
 from typer import Typer
 from rich import print
+from rich.table import Table
 from kamaqi.utils.files import read_project_file
 
 app=Typer(help="Show apps, endpoints and models")
@@ -7,10 +8,22 @@ app=Typer(help="Show apps, endpoints and models")
 @app.command(name="apps",
              help="Show apps and their metadata")
 def list_apps():
-    
+
     project_file = read_project_file()
     project_apps = project_file["apps"]
-    print(project_apps)
+    apps_table = Table("App","Status")
+
+    for app,data in project_apps.items():
+        app_status = data["status"].capitalize()
+        app_name = app.capitalize()
+        if app_status == "Upgraded":
+            app_status = f"[bold green]{app_status}[/bold green]"
+        if app_status == "Added":
+            app_status = f"[bold yellow]{app_status}[/bold yellow]"
+            
+        apps_table.add_row(app_name,app_status)
+
+    print(apps_table)
 
 @app.command(name="config",
             help="Show kamaqi.json")
